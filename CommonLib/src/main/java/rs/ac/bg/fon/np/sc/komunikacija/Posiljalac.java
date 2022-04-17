c96cffa52bb2b266cbf866bc0fb1e8a264ccca6b
UrosVesic <uros99uki@gmail.com>
2022-04-17 20:12:39 +0200

    Implementirana metoda posalji()

--- CommonLib/src/main/java/rs/ac/bg/fon/np/sc/komunikacija/Posiljalac.java
+++ CommonLib/src/main/java/rs/ac/bg/fon/np/sc/komunikacija/Posiljalac.java
@@ -1,5 +1,8 @@
 package rs.ac.bg.fon.np.sc.komunikacija;
 
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class Posiljalac {
@@ -7,5 +10,16 @@ public class Posiljalac {
 
 	public Posiljalac(Socket socket) {
 		this.socket = socket;
+	}
+
+	public void posalji(Object object) {
+		try {
+			ObjectOutputStream out = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+			out.writeObject(object);
+			out.flush();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
 	}
 }
