f2e0d89d60947bd3c25f41c94b23fa22ce8ec230
UrosVesic <uros99uki@gmail.com>
2022-04-17 20:03:48 +0200

    Dodata klasa Primalac

---
+++ CommonLib/src/main/java/rs/ac/bg/fon/np/sc/komunikacija/Primalac.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.np.sc.komunikacija;
+
+import java.net.Socket;
+
+public class Primalac {
+	Socket socket;
+
+	public Primalac(Socket socket) {
+		this.socket = socket;
+	}
+
+	
+}
