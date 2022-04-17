8de98b2c3dcd00a66d70bc8de54187efbea448e1
UrosVesic <uros99uki@gmail.com>
2022-04-17 18:34:23 +0200

    Napravljen projekat i interfejs OpstiDomenskiObjekat

---
+++ CommonLib/src/main/java/rs/ac/bg/fon/np/sc/commonLib/OpstiDomenskiObjekat.java
@@ -0,0 +1,52 @@
+package rs.ac.bg.fon.np.sc.commonLib;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public interface OpstiDomenskiObjekat {
+
+	String vratiVrednostiAtributa();
+
+	String postaviVrednostiAtributa();
+
+	String vratiImeKlase();
+
+	String vratiUslovZaNadjiSlog();
+
+	void napuni(ResultSet rs) throws SQLException;
+
+	String vratiNazivPK();
+
+	void povecajBroj(ResultSet rs) throws SQLException;
+
+	void postaviPocetniBroj();
+
+	public OpstiDomenskiObjekat kreirajInstancu();
+
+	public int vratiBrojVezanihObjekata();
+
+	public OpstiDomenskiObjekat vratiVezaniObjekat(int i);
+
+	public void postaviVrednostVezanogObjekta(OpstiDomenskiObjekat vezo, int i);
+
+	default String vratiUslovZaPromeniSlog() {
+		return vratiUslovZaNadjiSlog();
+	}
+
+	default int vratiBrojSlogovaVezanogObjekta(int i) {
+		return 1;
+	}
+
+	default OpstiDomenskiObjekat vratiSlogVezanogObjekta(int i, int j) {
+		return null;
+	}
+
+	public default String vratiUslovZaNadjiSlogove() {
+		return vratiUslovZaNadjiSlog();
+	}
+
+	public default void kreirajVezaniObjekat(int brojStavki, int i) {
+
+	}
+
+}
