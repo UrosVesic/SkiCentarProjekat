f4095cd2e948bbc0744200ac0780d914160c51fd
UrosVesic <uros99uki@gmail.com>
2022-04-21 17:05:10 +0200

    Dodata prva operacija PRIJAVI_SE

--- CommonLib/src/main/java/rs/ac/bg/fon/np/sc/commonLib/komunikacija/Operacije.java
+++ CommonLib/src/main/java/rs/ac/bg/fon/np/sc/commonLib/komunikacija/Operacije.java
@@ -1,5 +1,7 @@
 package rs.ac.bg.fon.np.sc.commonlib.komunikacija;
 
-public class Operacije {
+import java.io.Serializable;
 
+public class Operacije implements Serializable{
+     public static final int PRIJAVI_SE = 1;
 }
