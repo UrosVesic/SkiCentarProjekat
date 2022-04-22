d1b5746a4e518c6e900e21f263f479ccd2dcf2c6
UrosVesic <uros99uki@gmail.com>
2022-04-22 19:11:46 +0200

    Dodata nova operacija

--- CommonLib/src/main/java/rs/ac/bg/fon/np/sc/commonLib/komunikacija/Operacije.java
+++ CommonLib/src/main/java/rs/ac/bg/fon/np/sc/commonLib/komunikacija/Operacije.java
@@ -2,9 +2,11 @@ package rs.ac.bg.fon.np.sc.commonlib.komunikacija;
 
 import java.io.Serializable;
 
-public class Operacije implements Serializable{
-     public static final int PRIJAVI_SE = 1;
-     public static final int UCITAJ_LISTU_SKI_CENTARA = 2;
-     public static final int ZAPAMTI_SKI_CENTAR = 3;
-     public static final int PRETRAZI_SKI_KARTE = 4;
+public class Operacije implements Serializable {
+
+    public static final int PRIJAVI_SE = 1;
+    public static final int UCITAJ_LISTU_SKI_CENTARA = 2;
+    public static final int ZAPAMTI_SKI_CENTAR = 3;
+    public static final int PRETRAZI_SKI_KARTE = 4;
+    public static final int ZAPAMTI_STAZU = 5;
 }
