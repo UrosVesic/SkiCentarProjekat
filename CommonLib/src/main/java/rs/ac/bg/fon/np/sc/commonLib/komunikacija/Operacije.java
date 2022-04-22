174a4c0342ef3313a287a442acb20512da957b2b
UrosVesic <uros99uki@gmail.com>
2022-04-22 18:28:10 +0200

    Dodata operacija

--- CommonLib/src/main/java/rs/ac/bg/fon/np/sc/commonLib/komunikacija/Operacije.java
+++ CommonLib/src/main/java/rs/ac/bg/fon/np/sc/commonLib/komunikacija/Operacije.java
@@ -6,4 +6,5 @@ public class Operacije implements Serializable{
      public static final int PRIJAVI_SE = 1;
      public static final int UCITAJ_LISTU_SKI_CENTARA = 2;
      public static final int ZAPAMTI_SKI_CENTAR = 3;
+     public static final int PRETRAZI_SKI_KARTE = 4;
 }
