c2f1c004cdf4494fbd336fba176e6768bad0ff78
UrosVesic <uros99uki@gmail.com>
2022-04-24 19:08:18 +0200

    Dodate operacije zapamti,pretrazi i promeni ski centar

--- CommonLib/src/main/java/rs/ac/bg/fon/np/sc/commonLib/komunikacija/Operacije.java
+++ CommonLib/src/main/java/rs/ac/bg/fon/np/sc/commonLib/komunikacija/Operacije.java
@@ -6,11 +6,14 @@ public class Operacije implements Serializable {
 
     public static final int PRIJAVI_SE = 1;
     public static final int UCITAJ_LISTU_SKI_CENTARA = 2;
-    public static final int ZAPAMTI_SKI_CENTAR = 3;
+    public static final int ZAPAMTI_SKI_KARTU = 3;
     public static final int PRETRAZI_SKI_KARTE = 4;
     public static final int ZAPAMTI_STAZU = 5;
     public static final int PRETRAZI_STAZE = 6;
     public static final int UCITAJ_STAZU = 7;
     public static final int PROMENI_STAZU = 8;
     public static final int ZAPAMTI_ZICARU = 9;
+    public static final int ZAPAMTI_SKI_CENTAR = 10;
+    public static final int PRETRAZI_SKI_CENTAR = 11;
+    public static final int PROMENI_SKI_CENTAR = 12;
 }
