e687ec86ae3de2d5a940dd8a9b249820b0c6c610
UrosVesic <uros99uki@gmail.com>
2022-06-10 14:47:17 +0200

    Promena imena paketa

--- CommonLib/src/main/java/rs/ac/bg/fon/np/sc/commonLib/dto/SkiCentarDto.java
+++ CommonLib/src/main/java/rs/ac/bg/fon/np/sc/commonLib/dto/SkiCentarDto.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package rs.ac.bg.fon.np.sc.commonLib.dto;
+package rs.ac.bg.fon.np.sc.commonlib.dto;
 
 import com.google.gson.annotations.Expose;
 import java.util.List;
