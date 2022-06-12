c68aa2f6dfa3e744ecf39755659499dbee24e8bb
UrosVesic <uros99uki@gmail.com>
2022-06-12 15:03:20 +0200

    Dodat javadoc

--- CommonLib/src/main/java/rs/ac/bg/fon/np/sc/commonLib/dto/SkiCentarDto.java
+++ CommonLib/src/main/java/rs/ac/bg/fon/np/sc/commonLib/dto/SkiCentarDto.java
@@ -12,15 +12,27 @@ import rs.ac.bg.fon.np.sc.commonLib.domen.Staza;
 import rs.ac.bg.fon.np.sc.commonLib.domen.Zicara;
 
 /**
+ * Klasa koja predstavlja ski centar sa prosirenim informacijama. Ima referencu
+ * na domenski objekat ski centar i listu staza i zicara.
  *
  * @author UrosVesic
+ * @see rs.ac.bg.fon.np.sc.commonLib.domen.SkiCentar
  */
 public class SkiCentarDto {
 
+    /**
+     * Domenski objekat ski centar na koji se odnosi dto objekat
+     */
     @Expose
     private SkiCentar skiCentar;
+    /**
+     * Lista staza u ski centru
+     */
     @Expose
     private List<Staza> staze;
+    /**
+     * Lista zicara u ski centru
+     */
     @Expose
     private List<Zicara> zicare;
 
@@ -33,26 +45,56 @@ public class SkiCentarDto {
         this.zicare = zicare;
     }
 
+    /**
+     * Vraca domenski objekat ski centar za koji je vezan dto objekat
+     *
+     * @return Ski centar kao objekat klase SkiCentar
+     */
     public SkiCentar getSkiCentar() {
         return skiCentar;
     }
 
+    /**
+     * Postavlja vrednost polja skiCentar na zadatu vrednost
+     *
+     * @param skiCentar Vrednost na koju treba postaviti polje skiCentar
+     */
     public void setSkiCentar(SkiCentar skiCentar) {
         this.skiCentar = skiCentar;
     }
 
+    /**
+     * Vraca staze koje se nalaze u zadatom ski centru
+     *
+     * @return Staze kao java.util.List
+     */
     public List<Staza> getStaze() {
         return staze;
     }
 
+    /**
+     * Postavlja vrednost polja staze na zadatu vrednost
+     *
+     * @param staze Vrednost na koju treba postaviti polje staze
+     */
     public void setStaze(List<Staza> staze) {
         this.staze = staze;
     }
 
+    /**
+     * Vraca zicare koje se nalaze u zadatom ski centru
+     *
+     * @return Staze kao java.util.List
+     */
     public List<Zicara> getZicare() {
         return zicare;
     }
 
+    /**
+     * Postavlja vrednost polja zicare na zadatu vrednost
+     *
+     * @param zicare Vrednost na koju treba postaviti polje zicare
+     */
     public void setZicare(List<Zicara> zicare) {
         this.zicare = zicare;
     }
