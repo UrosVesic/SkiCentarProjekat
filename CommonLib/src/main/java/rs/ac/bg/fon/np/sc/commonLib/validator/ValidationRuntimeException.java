0c3f3240a6d4353856ef75f03122414a6262ec8a
UrosVesic <uros99uki@gmail.com>
2022-06-13 16:22:20 +0200

    Dodata klasa ValidationRuntimeException koja nasledjuje RuntimeException

---
+++ CommonLib/src/main/java/rs/ac/bg/fon/np/sc/commonLib/validator/ValidationRuntimeException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.ac.bg.fon.np.sc.commonLib.validator;
+
+/**
+ *
+ * @author UrosVesic
+ */
+public class ValidationRuntimeException extends RuntimeException {
+
+    public ValidationRuntimeException(String message) {
+        super(message);
+    }
+
+}
